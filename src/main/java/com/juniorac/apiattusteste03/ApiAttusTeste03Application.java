fe71a2423c6fd46e0353146150a72837b7768d79
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2024-04-26 17:55:26 -0300

    Subindo o projeto

---
+++ src/main/java/com/juniorac/apiattusteste03/ApiAttusTeste03Application.java
@@ -0,0 +1,13 @@
+package com.juniorac.apiattusteste03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAttusTeste03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAttusTeste03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniorac/apiattusteste03/ApiAttusTeste03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorac.apiattusteste03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAttusTeste03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
